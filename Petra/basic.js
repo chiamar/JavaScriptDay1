e8ebe3cd0b812cdb881ec83a5e85fc62c46e5239
Petra Christina <p.sedlaczek@drei.at>
2022-01-26 10:47:34 +0100

    Add Basic Exercise 2-4

---
+++ Petra/basic.js
@@ -0,0 +1,7 @@
+var message ="Hi, my name is Petra"
+console.log(message + add1)
+document.write(message + add2)
+
+var players = ['Martin','Thomas','Peter','Mathias','Niki']
+console.log("The most valuable player of the match is " + players[2] + ".")
+console.log("The most valuable player of the match is ".concat(players[2],"."))
